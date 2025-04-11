a320494387471e921ed9e807eab62ed4988c67b6
Johannes Jonasson <jo1623jos@cosmos2.int.lunarc>
2025-04-11 08:43:29 +0200

    testing batch script which continously prints output from abaqus

---
+++ hello_abaqus_output.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -t 1:00:00
+
+# Number of nodes
+#SBATCH -N 1
+#SBATCH --exclusive
+
+#SBATCH --ntasks-per-node=20
+#SBATCH -J Hello_Abaqus_HPC_World
+
+#SBATCH -o process_%j.out
+#SBATCH -e process_%j.err
+
+#SBATCH --mail-user=johannes.jonasson@construction.lth.se
+#SBATCH --mail-type=START,END
+
+cat $0
+
+cp -p new_input.inp $SNIC_TMP
+
+mkdir -p /home/jo1623jos/myDir/logs
+
+cd $SNIC_TMP
+
+module add abaqus/V6R2023x
+
+abaqus job=new_input memory=60000MB cpus=20 scratch=. interactive > /home/jo1623jos/myDir/logs/myjobfile.out 2>&1
+
+cp -p *.* $SLURM_SUBMIT_DIR
+
